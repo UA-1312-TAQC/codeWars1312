978963eea05ccb89a973f511de3ff387c0b80e77
Daniil Rusanov <daniil12099@gmail.com>
2024-12-04 23:51:04 +0200

    newAvg

--- src/main/java/org/academy/kata/implementation/daniilrusanov/Seven.java
+++ src/main/java/org/academy/kata/implementation/daniilrusanov/Seven.java
@@ -5,7 +5,17 @@ import org.academy.kata.ISeven;
 public class Seven implements ISeven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        if(arr == null)
+            throw new IllegalArgumentException();
+
+        double sum = 0;
+        for (double val : arr)
+            sum += val;
+
+        double res = navg * (arr.length + 1) - sum;
+        if (res < 0)
+            throw new IllegalArgumentException();
+        else return (long) Math.ceil(res);
     }
 
     @Override
