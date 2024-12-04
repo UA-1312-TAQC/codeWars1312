7da416968e5c5d0406bddf62fb91e5e5c3d18da1
NastiaDamian <nastuhta2005@gmail.com>
2024-12-04 21:07:41 +0300

    Add NastiaDamian/GapInPrimes

--- src/main/java/org/academy/kata/implementation/NastiaDamian/Five.java
+++ src/main/java/org/academy/kata/implementation/NastiaDamian/Five.java
@@ -7,7 +7,29 @@ import java.math.BigInteger;
 public class Five implements IFive {
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long previousPrime = -1;
+
+        for (long i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                if (previousPrime != -1 && i - previousPrime == g) {
+                    return new long[]{previousPrime, i};
+                }
+                previousPrime = i;
+            }
+        }
+        return null;
+    }
+
+    static boolean isPrime(long num) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+
+        long sqrtNum = (long) Math.sqrt(num);
+        for (long i = 3; i <= sqrtNum; i += 2) {
+            if (num % i == 0) return false;
+        }
+        return true;
     }
 
     @Override
