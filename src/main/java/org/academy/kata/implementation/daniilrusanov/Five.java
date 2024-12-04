f16a580c93397328f9db2e5addad2cbd531411ff
Daniil Rusanov <daniil12099@gmail.com>
2024-12-05 00:07:01 +0200

    gap

--- src/main/java/org/academy/kata/implementation/daniilrusanov/Five.java
+++ src/main/java/org/academy/kata/implementation/daniilrusanov/Five.java
@@ -7,7 +7,25 @@ import java.math.BigInteger;
 public class Five implements IFive {
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long prevPrime = -1;
+
+        for (long i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                if(prevPrime != i && i - prevPrime == g) {
+                    return new long[]{prevPrime, i};
+                }
+                prevPrime = i;
+            }
+        }
+        return null;
+    }
+
+    private static boolean isPrime(long num) {
+        if (num < 2) return false;
+        for (long i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
     }
 
     @Override
