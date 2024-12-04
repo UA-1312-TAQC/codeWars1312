5d935149f3dd7a3b96ea4915a17fc9ef479cf5bd
nestea4 <panchoshnaanastasia@gmail.com>
2024-12-04 22:36:35 +0300

    added the task stringToNumber

--- src/main/java/org/academy/kata/implementation/nestea4/Eight.java
+++ src/main/java/org/academy/kata/implementation/nestea4/Eight.java
@@ -31,7 +31,7 @@ public class Eight implements IEight {
 
     @Override
     public int stringToNumber(String str) {
-        return 0;
+        return Integer.valueOf(str);
     }
 
     @Override
