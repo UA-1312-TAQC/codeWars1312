058afbe702fa87f54962ec567021e57438cd3c4a
Daniil Rusanov <daniil12099@gmail.com>
2024-12-04 23:54:57 +0200

    balance

--- src/main/java/org/academy/kata/implementation/daniilrusanov/Six.java
+++ src/main/java/org/academy/kata/implementation/daniilrusanov/Six.java
@@ -10,7 +10,31 @@ public class Six implements ISix {
 
     @Override
     public String balance(String book) {
-        return "";
+        String clearStr = book.replaceAll("[^a-zA-Z0-9.\\s\n]", "");
+        String[] trimStr = clearStr.split("\\n+");
+        trimStr[0] = "Original Balance: " + trimStr[0];
+
+        double total = 0;
+        double average;
+        double balance = Double.parseDouble(trimStr[0].split(":")[1].trim());
+
+        for(int i = 1; i < trimStr.length; i++) {
+            String[] temp = trimStr[i].split("\\s+");
+            double expense = Double.parseDouble(temp[2]);
+            total += expense;
+            balance -= expense;
+            trimStr[i] = temp[0] + " " + temp[1] + " " + temp[2] + " Balance " + String.format("%.2f", balance);
+        }
+
+        average = total / (trimStr.length - 1);
+        StringBuilder res = new StringBuilder();
+        for(String line : trimStr) {
+            res.append(line).append("\\r\\n");
+        }
+        res.append("Total expense  ").append(String.format("%.2f", total)).append("\\r\\n");
+        res.append("Average expense  ").append(String.format("%.2f", average));
+
+        return res.toString();
     }
 
     @Override
