a4ffe980a16c7c424ded6907610d0f480238521c
nestea4 <panchoshnaanastasia@gmail.com>
2024-12-04 23:11:13 +0300

    added the task seriesSum

--- src/main/java/org/academy/kata/implementation/nestea4/Seven.java
+++ src/main/java/org/academy/kata/implementation/nestea4/Seven.java
@@ -10,6 +10,11 @@ public class Seven implements ISeven {
 
     @Override
     public String seriesSum(int n) {
-        return "";
+        if (n==0) return "0.00";
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += 1.0/(1+i*3);
+        }
+        return String.format("%.2f",sum);
     }
 }
