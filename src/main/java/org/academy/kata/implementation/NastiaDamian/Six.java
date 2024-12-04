2b18535ffee5586b27026ceff1eb4020d2740ed4
NastiaDamian <nastuhta2005@gmail.com>
2024-12-04 19:56:12 +0300

    Add NastiaDamian/nbaCup

--- src/main/java/org/academy/kata/implementation/NastiaDamian/Six.java
+++ src/main/java/org/academy/kata/implementation/NastiaDamian/Six.java
@@ -2,8 +2,10 @@ package org.academy.kata.implementation.NastiaDamian;
 
 import org.academy.kata.ISix;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Six implements ISix {
     @Override
@@ -103,7 +105,35 @@ public class Six implements ISix {
 
     @Override
     public String nbaCup(String resultSheet, String toFind) {
-        return "";
+        if (toFind.isEmpty()) return "";
+
+        List<String[]> list = Arrays.stream(resultSheet.split(","))
+                .filter(a -> a.contains(toFind))
+                .map(x -> (x.replaceAll("(.*[0-9]+[.,]?[0-9]*)(\\s)(.*[0-9]+[.,]?[0-9]*)", "$1;$3")).split(";"))
+                .collect(Collectors.toList());
+
+        int wins = 0, draws = 0, losses = 0, scored = 0, conceded = 0;
+
+        for (String[] teamRes : list) {
+            int scoreOurTeam = 0, score2 = 0;
+
+            if (teamRes[0].substring(teamRes[0].lastIndexOf(" ") + 1).contains(".") || teamRes[1].substring(teamRes[1].lastIndexOf(" ") + 1).contains("."))
+                return "Error(float number):" + String.join(" ", teamRes);
+
+            if (!(teamRes[0].substring(0, teamRes[0].lastIndexOf(" "))).equals(toFind) && !(teamRes[1].substring(0, teamRes[1].lastIndexOf(" "))).equals(toFind))
+                return toFind + ":This team didn't play!";
+
+            scoreOurTeam = teamRes[0].contains(toFind) ? Integer.parseInt(teamRes[0].substring(teamRes[0].lastIndexOf(" ") + 1)) : Integer.parseInt(teamRes[1].substring(teamRes[1].lastIndexOf(" ") + 1));
+            score2 = !teamRes[0].contains(toFind) ? Integer.parseInt(teamRes[0].substring(teamRes[0].lastIndexOf(" ") + 1)) : Integer.parseInt(teamRes[1].substring(teamRes[1].lastIndexOf(" ") + 1));
+
+            wins += scoreOurTeam > score2 ? 1 : 0;
+            losses += scoreOurTeam < score2 ? 1 : 0;
+            draws += scoreOurTeam == score2 ? 1 : 0;
+            scored += scoreOurTeam;
+            conceded += score2;
+        }
+
+        return String.format("%s:W=%d;D=%d;L=%d;Scored=%d;Conceded=%d;Points=%d", toFind, wins, draws, losses, scored, conceded, wins * 3 + draws);
     }
 
     @Override
