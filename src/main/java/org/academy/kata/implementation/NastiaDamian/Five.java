69f95321dec3e9959426bc8868104e23258b784a
NastiaDamian <nastuhta2005@gmail.com>
2024-12-04 21:20:06 +0300

    Add NastiaDamian/smallest

--- src/main/java/org/academy/kata/implementation/NastiaDamian/Five.java
+++ src/main/java/org/academy/kata/implementation/NastiaDamian/Five.java
@@ -27,6 +27,27 @@ public class Five implements IFive {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        long min = n;
+        int indexI = 0;
+        int indexJ = 0;
+        String number = String.valueOf(n);
+
+        for (int i = 0; i < number.length(); i++) {
+            for (int j = 0; j < number.length(); j++) {
+                if (i != j) {
+                    StringBuilder newNumb = new StringBuilder(number);
+                    char c = newNumb.charAt(i);
+                    newNumb.deleteCharAt(i);
+                    newNumb.insert(j, c);
+                    long newNumber = Long.parseLong(newNumb.toString());
+                    if (newNumber < min) {
+                        min = newNumber;
+                        indexI = i;
+                        indexJ = j;
+                    }
+                }
+            }
+        }
+        return new long[]{min, indexI, indexJ};
     }
 }
