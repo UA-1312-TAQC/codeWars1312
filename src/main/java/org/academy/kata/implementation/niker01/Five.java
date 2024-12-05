70eb54540647ad0d6d163272d1c86428b5ec059b
niker01 <niker234567@gmail.com>
2024-12-05 15:05:28 +0200

    Add gap

--- src/main/java/org/academy/kata/implementation/niker01/Five.java
+++ src/main/java/org/academy/kata/implementation/niker01/Five.java
@@ -7,7 +7,27 @@ import java.math.BigInteger;
 public class Five implements IFive {
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+         boolean[] sieve = new boolean[(int) n + 1];
+        Arrays.fill(sieve, true);
+        sieve[0] = sieve[1] = false;
+        
+        for (long i = 2; i * i <= n; i++) {
+            if (sieve[(int) i]) {
+                for (long j = i * i; j <= n; j += i) {
+                    sieve[(int) j] = false;
+                }
+            }
+        }
+        long prevPrime = -1;
+        for (long i = m; i <= n; i++) {
+            if (sieve[(int) i]) {
+                if (prevPrime != -1 && i - prevPrime == g) {
+                    return new long[] { prevPrime, i };
+                }
+                prevPrime = i;
+            }
+        }
+        return null;  
     }
 
     @Override
