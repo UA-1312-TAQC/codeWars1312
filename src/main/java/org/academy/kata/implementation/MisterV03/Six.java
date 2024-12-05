747c8e16e1432d2f65b7016725073728b4c11688
Viktor Kravchenko <viktorkrav03@gmail.com>
2024-12-05 11:39:41 +0200

    Task Help the bookseller completed

--- src/main/java/org/academy/kata/implementation/MisterV03/Six.java
+++ src/main/java/org/academy/kata/implementation/MisterV03/Six.java
@@ -2,7 +2,27 @@ package org.academy.kata.implementation.MisterV03;
 
 import org.academy.kata.ISix;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Six implements ISix {
+
+
+    public static String format(Map<String, Integer> in) {
+        List<String> elements = new ArrayList<>(in.keySet());
+//		Collections.sort(elements);
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i<elements.size(); i++) {
+            builder.append("(").append(elements.get(i)).append(" : ").append(in.get(elements.get(i))).append(")");
+            if(i+1<elements.size()) {
+                builder.append(" - ");
+            }
+        }
+        return builder.toString();
+    }
+
     @Override
     public long findNb(long m) {
         return 0;
@@ -35,6 +55,20 @@ public class Six implements ISix {
 
     @Override
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return "";
+        if(lstOfArt.length == 0 || lstOf1stLetter.length == 0)
+            return "";
+        Map<String, Integer> res = new HashMap<>();
+        for (String s : lstOf1stLetter) {
+            res.put(s, 0);
+        }
+        for (String s : lstOfArt) {
+            String temp = s.substring(0, 1);
+            if (res.get(temp) == null)
+                continue;
+            int spaceIndex = s.indexOf(" ");
+            String number = s.substring(spaceIndex + 1);
+            res.put(temp, res.get(temp) + Integer.parseInt(number));
+        }
+        return format(res);
     }
 }
