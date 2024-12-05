4e08609f08e7ce5c79d9ca86579c490c1aa35aec
nestea4 <panchoshnaanastasia@gmail.com>
2024-12-05 14:34:35 +0300

    added the task smallest

--- src/main/java/org/academy/kata/implementation/nestea4/Five.java
+++ src/main/java/org/academy/kata/implementation/nestea4/Five.java
@@ -27,6 +27,28 @@ public class Five implements IFive {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        String numStr = String.valueOf(n);
+        int len = numStr.length();
+
+        long smallestNum = n;
+        int takeIdx = 0;
+        int putIdx = 0;
+        for (int i=0; i<len; i++) {
+            for (int j=0; j<len; j++) {
+                StringBuilder sb = new StringBuilder(numStr);
+                char digit = sb.charAt(i);
+                sb.deleteCharAt(i);
+                sb.insert(j, digit);
+
+                long newNum = Long.parseLong(sb.toString());
+                if (newNum<smallestNum || (newNum == smallestNum && (i<takeIdx || (i == takeIdx && j<putIdx)))) {
+                    smallestNum = newNum;
+                    takeIdx = i;
+                    putIdx = j;
+                }
+            }
+        }
+
+        return new long[] {smallestNum,takeIdx,putIdx};
     }
 }
