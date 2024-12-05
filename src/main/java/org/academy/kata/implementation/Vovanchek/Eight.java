0a3702a0f9e175558fc9403f2c94c0e5aa049109
Vova K. <101831233+Vovanchek@users.noreply.github.com>
2024-12-05 12:20:46 +0200

    The task am_i_wilson was updated

--- src/main/java/org/academy/kata/implementation/Vovanchek/Eight.java
+++ src/main/java/org/academy/kata/implementation/Vovanchek/Eight.java
@@ -45,7 +45,7 @@ public class Eight implements IEight {
     }
 
     @Override
-    public boolean am_i_wilson(double n) {
+    public boolean am_i_wilson(long n) {
          if (n <= 1) {
             return false;
         }
