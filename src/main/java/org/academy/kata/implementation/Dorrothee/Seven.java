200b1558953e2c3941d10fd48a7cdf16f944d25d
Daryna Boruk <darynaboruk@gmail.com>
2024-12-11 19:09:21 +0200

    added seriesSum implementation

--- src/main/java/org/academy/kata/implementation/Dorrothee/Seven.java
+++ src/main/java/org/academy/kata/implementation/Dorrothee/Seven.java
@@ -2,6 +2,9 @@ package org.academy.kata.implementation.Dorrothee;
 
 import org.academy.kata.ISeven;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Seven implements ISeven {
     @Override
     public long newAvg(double[] arr, double navg) {
@@ -10,6 +13,19 @@ public class Seven implements ISeven {
 
     @Override
     public String seriesSum(int n) {
-        return "";
+        if (n == 0) { return "0.00"; }
+
+        float result = 0;
+        float denominator = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result += 1 / denominator;
+            denominator += 3;
+        }
+
+        BigDecimal resultRounded = new BigDecimal(result);
+        resultRounded = resultRounded.setScale(2, RoundingMode.HALF_UP);
+
+        return resultRounded.toString();
     }
 }
