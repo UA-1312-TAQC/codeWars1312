bede18141e6893aea00cf1cec1ec7ff2b7929916
Daryna Boruk <darynaboruk@gmail.com>
2024-12-11 19:12:48 +0200

    added findNb implementation

--- src/main/java/org/academy/kata/implementation/Dorrothee/Six.java
+++ src/main/java/org/academy/kata/implementation/Dorrothee/Six.java
@@ -5,7 +5,19 @@ import org.academy.kata.ISix;
 public class Six implements ISix {
     @Override
     public long findNb(long m) {
-        return 0;
+        long volume = 0;
+        long n = 0;
+
+        while (volume < m) {
+            n++;
+            volume += (long) Math.pow(n, 3); // (n−1)^3
+        }
+
+        if (volume == m) {
+            return n;
+        } else {
+            return -1;
+        }
     }
 
     @Override
