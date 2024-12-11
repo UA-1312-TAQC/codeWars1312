5713313ed2204c952983bb30241254dc9dfe9f8a
Daryna Boruk <darynaboruk@gmail.com>
2024-12-11 17:45:53 +0200

    added mpgToKPM implementation

--- src/main/java/org/academy/kata/implementation/Dorrothee/Eight.java
+++ src/main/java/org/academy/kata/implementation/Dorrothee/Eight.java
@@ -3,6 +3,9 @@ package org.academy.kata.implementation.Dorrothee;
 
 import org.academy.kata.IEight;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Eight implements IEight {
     @Override
     public int liters(double time) {
@@ -19,7 +22,17 @@ public class Eight implements IEight {
 
     @Override
     public float mpgToKPM(float mpg) {
-        return 0;
+        final float mile = 1.609344f;
+        final float gallon = 4.54609188f;
+
+        float kilosPerLiter = mpg * mile / gallon;
+
+        //To round off the result
+        BigDecimal bd = new BigDecimal(kilosPerLiter);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        float kpl = bd.floatValue();
+
+        return kpl;
     }
 
     @Override
