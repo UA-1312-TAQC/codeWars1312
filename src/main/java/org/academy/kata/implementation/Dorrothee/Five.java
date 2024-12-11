44da79004d4420bbbfc9cccdc34d00b3286287ad
Daryna Boruk <darynaboruk@gmail.com>
2024-12-11 19:43:24 +0200

    added smallest implementation

--- src/main/java/org/academy/kata/implementation/Dorrothee/Five.java
+++ src/main/java/org/academy/kata/implementation/Dorrothee/Five.java
@@ -27,6 +27,34 @@ public class Five implements IFive {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        String strNumber = Long.toString(n);
+        long number = n;
+        int taken = -1;
+        int inserted = -1;
+
+        //Go through each digit in the number to find the smallest "combination"
+        for (int i = 0; i < strNumber.length(); i++) {
+            char digit = strNumber.charAt(i);
+            StringBuilder sb = new StringBuilder(strNumber);
+            sb.deleteCharAt(i);  //Remove the digit at index i
+
+            //Inserting the digit at each position j to find the smallest "combination"
+            for (int j = 0; j <= sb.length(); j++) {
+                sb.insert(j, digit);
+                long newNumber = Long.parseLong(sb.toString());
+
+                //Update the number value if the new number is smaller
+                if (newNumber < number) {
+                    number = newNumber;
+                    taken = i;
+                    inserted = j;
+                }
+
+                //Restore the string for the next iteration
+                sb.deleteCharAt(j);
+            }
+        }
+
+        return new long[] {number, taken, inserted};
     }
 }
