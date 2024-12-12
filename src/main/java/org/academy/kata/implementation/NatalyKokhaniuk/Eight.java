673771d9447aac372e065af222fbc8138a69f496
Nataly Kokhaniuk <kokhaniuk.nataliia@chnu.edu.ua>
2024-12-12 14:20:15 +0200

    NatalyKokhaniuk/wilson

--- src/main/java/org/academy/kata/implementation/NatalyKokhaniuk/Eight.java
+++ src/main/java/org/academy/kata/implementation/NatalyKokhaniuk/Eight.java
@@ -46,9 +46,39 @@ public class Eight implements IEight {
     public int[] divisibleBy(int[] numbers, int divider) {
         return new int[0];
     }
+    private static boolean isPrime(long n) {
+            if (n <= 1) {
+                return false;
+            }
+            for (long i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
 
+        private static long factorialMod(long n, long mod) {
+            long result = 1;
+            for (long i = 1; i <= n; i++) {
+                result = (result * i) % mod;
+            }
+            return result;
+        }
     @Override
     public boolean am_i_wilson(long n) {
-        return false;
+        if (!isPrime(n)) {
+                System.out.println("\"" + n + "\" isn't a prime. Try another number.");
+                return false;
+            }
+            long mod = n * n;
+            long factorial = factorialMod(n - 1, mod);
+            if ((factorial + 1) % mod == 0) {
+                System.out.println("TRUE \"" + n + "\" is a Wilson prime.");
+                return true;
+            } else {
+                System.out.println("FALSE \"" + n + "\" isn't a Wilson prime.");
+                return false;
+            }
     }
 }
