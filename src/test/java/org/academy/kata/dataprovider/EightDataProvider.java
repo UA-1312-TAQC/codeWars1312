a8fae7d5188a95d34229c3ac632147be5e85d128
Daryna Boruk <darynaboruk@gmail.com>
2024-12-12 20:11:59 +0200

    added testGetVolumeOfCuboid implementation

--- src/test/java/org/academy/kata/dataprovider/EightDataProvider.java
+++ src/test/java/org/academy/kata/dataprovider/EightDataProvider.java
@@ -33,4 +33,23 @@ public class EightDataProvider extends BaseDataProvider {
         inputs.add(new Object[]{15.5f, 5.46f});
         return combineData(inputs, EIGHTS);
     }
+
+    @DataProvider(name = "data-Cuboid")
+    public static Iterator<Object[]> dpCuboid() {
+        final List<Object[]> inputs = new ArrayList<>();
+        // Positive tests
+        inputs.add(new Object[]{1, 2, 2, 4});
+        inputs.add(new Object[]{6.3, 2, 5, 63});
+        inputs.add(new Object[]{4, 5, 6, 120});
+        inputs.add(new Object[]{1.5, 2.5, 3, 11.25});
+        inputs.add(new Object[]{3, 3, 3, 27});
+        // Negative tests
+        inputs.add(new Object[]{'a', 2, 3, 6});
+        inputs.add(new Object[]{-1, 2, 3, 0});
+        inputs.add(new Object[]{Double.NaN, 2, 3, 6});
+        inputs.add(new Object[]{1e308, 2.0, Double.MIN_VALUE, 0.0});
+        inputs.add(new Object[]{1, 2, 2, 5});
+
+        return combineData(inputs, EIGHTS);
+    }
 }
