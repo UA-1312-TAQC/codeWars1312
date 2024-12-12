eb4f974ba960ecc2ecb6e4ba7a5ac8302e4a6dc4
Nataly Kokhaniuk <kokhaniuk.nataliia@chnu.edu.ua>
2024-12-12 14:24:29 +0200

    NatalyKokhaniuk/seriesSum

--- src/main/java/org/academy/kata/implementation/NatalyKokhaniuk/Seven.java
+++ src/main/java/org/academy/kata/implementation/NatalyKokhaniuk/Seven.java
@@ -10,6 +10,25 @@ public class Seven implements ISeven {
 
     @Override
     public String seriesSum(int n) {
-        return "";
+       if(n==0)return "0.00";
+        if(n<1) {
+            System.out.println("\""+n+"\" should be a natural number .");
+            return null;
+        }
+        double res=1.00;
+        int nn=4;
+        String x, resStr=n+" --> 1 ";
+        for(int i=1;i<n;i++){
+            resStr+="+ 1/"+nn+" ";
+            res+=1.0/nn;
+            nn+=3;
+        }
+        res=Math.round(res*100.0)/100.00;
+        x=""+res;
+        if(Math.round(res)==res) x= Math.round(res)+".00";
+        else if(Math.round(res*10)==10*res) x= res+"0";
+        resStr+=" --> \""+x+"\"";
+        System.out.println(resStr);
+        return x;
     }
 }
