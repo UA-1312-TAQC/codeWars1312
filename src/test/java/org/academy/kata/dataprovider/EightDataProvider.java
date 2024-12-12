dcdee563482dfc5c8d16df85d0a645daebfd1311
tanya_bilka <tanyawbw7@gmail.com>
2024-12-12 23:09:22 +0200

    The "testLiters" was changed

--- src/test/java/org/academy/kata/dataprovider/EightDataProvider.java
+++ src/test/java/org/academy/kata/dataprovider/EightDataProvider.java
@@ -11,25 +11,20 @@ public class EightDataProvider extends BaseDataProvider {
     @DataProvider(name = "data-Liters")
     public static Iterator<Object[]> dpLiters() {
         final List<Object[]> inputs = new ArrayList<>();
-        inputs.add(new Object[]{5, 2});
-        inputs.add(new Object[]{55, 27});
-        inputs.add(new Object[]{2, 1});
-        inputs.add(new Object[]{0.97, 0});
-        inputs.add(new Object[]{14.64, 7});
-        inputs.add(new Object[]{1600.20, 800});
-        inputs.add(new Object[]{80, 40});
-        inputs.add(new Object[]{0, 0});
-        inputs.add(new Object[]{Integer.MAX_VALUE * 2.0, 2147483647});
-
-        return combineData(inputs, EIGHTS);
-    }
-
-    @DataProvider(name = "data-Liters_neg")
-    public static Iterator<Object[]> dpLiters_neg() {
-        final List<Object[]> inputs = new ArrayList<>();
-        inputs.add(new Object[]{-1, -1});
-        inputs.add(new Object[]{-77.8, -39});
-        inputs.add(new Object[]{Integer.MAX_VALUE * 2.0 + 1, 0});
+        //positive
+        inputs.add(new Object[]{5, 2, false});
+        inputs.add(new Object[]{55, 27, false});
+        inputs.add(new Object[]{2, 1, false});
+        inputs.add(new Object[]{0.97, 0, false});
+        inputs.add(new Object[]{14.64, 7, false});
+        inputs.add(new Object[]{1600.20, 800, false});
+        inputs.add(new Object[]{80, 40, false});
+        inputs.add(new Object[]{0, 0, false});
+        inputs.add(new Object[]{Integer.MAX_VALUE * 2.0, 2147483647, false});
+        //negative
+        inputs.add(new Object[]{-1, -1, true});
+        inputs.add(new Object[]{-77.8, -39, true});
+        inputs.add(new Object[]{Integer.MAX_VALUE * 2.0 + 1, 0, true});
 
         return combineData(inputs, EIGHTS);
     }
