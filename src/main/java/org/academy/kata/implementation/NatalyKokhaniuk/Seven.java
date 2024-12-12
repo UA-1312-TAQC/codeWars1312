39ce2ed0bb3698df5d7e726246cf252340331ed0
Nataly Kokhaniuk <kokhaniuk.nataliia@chnu.edu.ua>
2024-12-12 14:23:37 +0200

    NatalyKokhaniuk/newAvg

--- src/main/java/org/academy/kata/implementation/NatalyKokhaniuk/Seven.java
+++ src/main/java/org/academy/kata/implementation/NatalyKokhaniuk/Seven.java
@@ -5,7 +5,24 @@ import org.academy.kata.ISeven;
 public class Seven implements ISeven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        if(navg<=0) {
+            System.out.println("\""+navg+"\" should be positive number .");
+            return -1;
+        }
+        String resStr="if donations = [";
+        double res= navg * (arr.length+1);
+        for(int i=0;i<arr.length;++i){
+            if(arr[i]<0){
+                System.out.println("All donations should be positive numbers .");
+                return -1;}
+            resStr+=arr[i];
+            if(i != arr.length-1)resStr+=", ";
+            res-=arr[i];
+        }
+        resStr+="] then new_avg(donations, "+ Math.round(navg) +") --> "+Math.round(res);
+        System.out.println(resStr);
+       if(res<0) throw new IllegalArgumentException();
+       return Math.round(res);
     }
 
     @Override
