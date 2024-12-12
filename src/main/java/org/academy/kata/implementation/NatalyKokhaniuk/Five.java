6688ab4640f55f12a29780819c09af8939d81c78
Nataly Kokhaniuk <kokhaniuk.nataliia@chnu.edu.ua>
2024-12-12 14:36:55 +0200

    NatalyKokhaniuk/ToSmallest

--- src/main/java/org/academy/kata/implementation/NatalyKokhaniuk/Five.java
+++ src/main/java/org/academy/kata/implementation/NatalyKokhaniuk/Five.java
@@ -27,6 +27,60 @@ public class Five implements IFive {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+       String numStr = Long.toString(n);
+        List<Character> digits = new ArrayList<>();
+        
+        for (char c : numStr.toCharArray()) {
+            digits.add(c);
+        }
+        
+        long minNumber = n;   
+        int bestI = -1;       
+        int bestJ = -1;       
+
+        for (int i = 0; i < digits.size(); i++) {
+            char digit = digits.get(i);  
+            
+            for (int j = 0; j <= digits.size(); j++) {
+                if (i == j) continue; 
+                
+                if (j == digits.size()) {
+                    List<Character> newDigits = new ArrayList<>(digits);
+                    newDigits.remove(i);   
+                    newDigits.add(digit);  
+                    StringBuilder newNumStr = new StringBuilder();
+                    for (char c : newDigits) {
+                        newNumStr.append(c);
+                    }
+                    long newNumber = Long.parseLong(newNumStr.toString());
+                    
+                    if (newNumber < minNumber) {
+                        minNumber = newNumber;
+                        bestI = i;
+                        bestJ = j;
+                    }
+                    continue;
+                }
+
+                List<Character> newDigits = new ArrayList<>(digits);
+                newDigits.remove(i);   
+                newDigits.add(j, digit);  
+                
+                StringBuilder newNumStr = new StringBuilder();
+                for (char c : newDigits) {
+                    newNumStr.append(c);
+                }
+                
+                long newNumber = Long.parseLong(newNumStr.toString());
+
+                if (newNumber < minNumber) {
+                    minNumber = newNumber;
+                    bestI = i;
+                    bestJ = j;
+                }
+            }
+        }
+        System.out.println("smallest("+n+") --> ["+minNumber+", "+bestI+", "+bestJ+"]");
+        return new long[]{minNumber, bestI, bestJ};
     }
 }
