1c93bec649d369af2c45807c5e29198533d8878d
Nataly Kokhaniuk <kokhaniuk.nataliia@chnu.edu.ua>
2024-12-12 14:13:00 +0200

    Update Eight.java

--- src/main/java/org/academy/kata/implementation/NatalyKokhaniuk/Eight.java
+++ src/main/java/org/academy/kata/implementation/NatalyKokhaniuk/Eight.java
@@ -11,7 +11,10 @@ public class Eight implements IEight {
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return 0;
+         if(length<=0||width<=0||height<=0) return -1;
+        double res=length * width * height;
+        System.out.println("Volume of cuboid: " + res);
+        return  res;
     }
 
     @Override
