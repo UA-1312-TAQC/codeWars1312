4a5e18072a91e5c6f060b5c0182162e17d8cf3dc
nestea4 <panchoshnaanastasia@gmail.com>
2024-12-13 01:43:14 +0300

    added testNewAvg and dpNewAvg

--- src/test/java/org/academy/kata/SevenTest.java
+++ src/test/java/org/academy/kata/SevenTest.java
@@ -1,12 +1,24 @@
 package org.academy.kata;
 
 import org.academy.kata.dataprovider.SevenDataProvider;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SevenTest extends SevenDataProvider {
 
-    @Test
-    public void testNewAvg() {
+    @Test(dataProvider = "data-newAvg")
+    public void testNewAvg(ISeven seven, double[] arr, double navg, long expected, boolean expectException) {
+        if (!expectException) {
+            long actual = seven.newAvg(arr, navg);
+            Assert.assertEquals(actual, expected);
+        } else {
+            try {
+                seven.newAvg(arr, navg);
+                Assert.fail("Expected IllegalArgumentException was't thrown");
+            } catch (IllegalArgumentException ex) {
+              //тест проходить, якщо  exception зловлено
+            }
+        }
     }
 
     @Test
--- src/test/java/org/academy/kata/dataprovider/SevenDataProvider.java
+++ src/test/java/org/academy/kata/dataprovider/SevenDataProvider.java
@@ -1,4 +1,29 @@
 package org.academy.kata.dataprovider;
 
+import org.testng.annotations.DataProvider;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class SevenDataProvider extends BaseDataProvider{
+    @DataProvider(name = "data-newAvg")
+    public static Iterator<Object[]> dpNewAvg() {
+        final List<Object[]> inputs = new ArrayList<>();
+
+        inputs.add(new Object[]{new double[]{14, 30, 5, 7, 9, 11, 15}, 30, 149, false});
+        inputs.add(new Object[]{new double[]{14, 30, 5, 7, 9, 11, 15}, 92, 645, false});
+        inputs.add(new Object[]{new double[]{10}, 20, 30, false});
+        inputs.add(new Object[]{new double[]{}, 50, 50, false});
+        inputs.add(new Object[]{new double[]{1, 2, 3, 4, 5}, 10, 45, false});
+
+        inputs.add(new Object[]{new double[]{14, 30, 5, 7, 9, 11, 15}, 2, 0, true});
+        inputs.add(new Object[]{new double[]{100, 100, 100}, 10, 0, true});
+        inputs.add(new Object[]{new double[]{50, 50, 50, 50, 50}, 20, 0, true});
+        inputs.add(new Object[]{new double[]{1000}, 1, 0, true});
+        inputs.add(new Object[]{new double[]{10, 20, 30, 40, 50}, 5, 0, true});
+
+        return combineData(inputs, SEVENTH);
+    }
+
 }
