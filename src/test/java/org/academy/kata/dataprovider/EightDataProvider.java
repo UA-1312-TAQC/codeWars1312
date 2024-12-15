adc46fa9ca07dba70bbc1aee4966d7f92b76ec40
Viktor Kravchenko <viktorkrav03@gmail.com>
2024-12-15 19:08:46 +0200

    Tests for TwoDecіmalsPlaces and findTheSmalleste created

--- src/test/java/org/academy/kata/dataprovider/EightDataProvider.java
+++ src/test/java/org/academy/kata/dataprovider/EightDataProvider.java
@@ -17,4 +17,17 @@ public class EightDataProvider extends BaseDataProvider{
         return combineData(inputs, EIGHTS);
 
     }
+
+
+    @DataProvider(name = "twoDecimalPlacesData")
+    public static Iterator<Object[]> testTwoDecimalPlacesData() {
+        final List<Object[]> inputs = new ArrayList<>();
+        inputs.add(new Object[]{173735326.38, 173735326.3783732637948948, 0.00});
+        inputs.add(new Object[]{14.66, 14.659725356, 0.00});
+        inputs.add(new Object[]{48.92, 48.9191111131, 0.00});
+        inputs.add(new Object[]{100.01, 100.009, 0.00});
+        inputs.add(new Object[]{100., 100.0009, 0.00});
+        inputs.add(new Object[]{100.01, 100.005, 0.00});
+        return inputs.iterator();
+    }
 }
